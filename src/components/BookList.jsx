11b7fc2c59f43c1c8d48eade7b12071048cf56e9
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-04-04 12:09:26 +0000

    Setup reducers and actions

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,27 @@
 const BookList = () => (
-  <div className="px-10 mx-24  card card-side bg-base-100">
-    <div className="card-body grid grid-cols-3">
+  <div className="px-10 mx-24 card card-side bg-base-100">
+    <div className="grid grid-cols-3 card-body">
       <ul>
         <li className="text-sm font-extrabold text-[#8c8c8c]">Action</li>
         <li className="text-2xl font">Grief Child</li>
         <li className="text-sm">Shafiu Yushawu</li>
         <li>
-          <ul className="flex gap-5">
-            <li>Comments</li>
-            <li>Remove</li>
-            <li>Edit</li>
+          <ul className="flex gap-2">
+            <li>
+              <button type="button" className="btn btn-sm">
+                Comments
+              </button>
+            </li>
+            <li>
+              <button type="button" className="btn btn-sm">
+                Remove
+              </button>
+            </li>
+            <li>
+              <button type="button" className="btn btn-sm">
+                Remove
+              </button>
+            </li>
           </ul>
         </li>
       </ul>
@@ -27,7 +39,7 @@ const BookList = () => (
           </ul>
         </li>
       </ul>
-      <ul className=" flex flex-col gap-5">
+      <ul className="flex flex-col gap-5 ">
         <li className="text-[#979797]">Current Chapter</li>
         <li>Chapter 17</li>
         <li>
